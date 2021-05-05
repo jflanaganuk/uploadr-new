f5822601b300ffc8c515368134240aff3cd5831a
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-05-05 16:14:15 +0100

    Added typing effect

--- src/header.tsx
+++ src/header.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Nav } from './nav';
 
+import './header.scss'
+
 export const Header = () => {
 
     return (
         <header>
             <h1>Joshua Flanagan</h1>
             <h2>Web Developer - Nottingham 🇬🇧</h2>
-            <h3>React | Typescript | Threejs | Node</h3>
+            <h3><span className="autoTyper"></span></h3>
             <Nav/>
         </header>
     )
